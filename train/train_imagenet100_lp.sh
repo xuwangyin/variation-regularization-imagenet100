4c20fdc90285c3cf8595e412a538e476a36a6592
Xuwang Yin <xuwangyin@gmail.com>
2023-06-05 15:27:05 -0700

    L2+VR training logs (not finished)

--- train/train_imagenet100_lp.sh
+++ train/train_imagenet100_lp.sh
@@ -1,6 +1,10 @@
+# Based on https://github.com/inspire-group/variation-regularization/tree/main/train
 python="/data/$(whoami)/anaconda3/envs/corruptions/bin/python"
 
-srun --job-name="imagenet100-lp-vr" --time=2-00:00:00 --gpus=1  \
-$python -u train_imagenette.py --epochs 200 --var-reg-type var \
-    --norm l_2 --epsilon 128 --pgd-alpha 19.125 --var-reg 1 \
-    --data-dir /data/$(whoami)/Projects/adversarial-corruptions/datasets/imagenet100 --logits-reg --fname imagenet100_lp_vr | tee imagenet100_lp_vr.log
+for reg in 1.0 0 0.1 0.5 0.3; do
+    sleep 2
+    srun --job-name="imagenet100-l2-vr-$reg" --time=2-00:00:00 --gpus=4 --nodes=1 \
+        $python -u train_imagenette.py --var-reg-type var \
+        --norm l_2 --epsilon 128 --pgd-alpha 19.125 --var-reg $reg \
+        --data-dir /data/$(whoami)/Projects/adversarial-corruptions/datasets/imagenet100 --logits-reg --fname imagenet100_l2_vr_$reg
+done
